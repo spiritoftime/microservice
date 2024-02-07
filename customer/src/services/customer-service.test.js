0ecee25ab8d6463dc8060539f68d932daeaf3b0e
spiritoftime <chooliangyi6@gmail.com>
2024-02-07 15:53:50 +0800

    add change

---
+++ customer/src/services/customer-service.test.js
@@ -0,0 +1,7 @@
+describe("CustomerService", () => {
+  // which function we are testing
+  describe("SignIn", () => {
+    test("validate user inputs", () => {});
+    test("validate response", async () => {});
+  });
+});
